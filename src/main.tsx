9d9a5ff373fff07cb0c35a9a2775c816b777a763
sergei <sergei42ru.alekseenko@yandex.ru>
2025-06-26 00:43:01 +0700

    fix: деплой

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import './index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'; // лучше именно react-router-dom
 
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/Todo">
             <App />
         </BrowserRouter>
     </StrictMode>,
