7d90fb8219ec6da37c7c16d51b7f5ecf51ebfb77
sergei <sergei42ru.alekseenko@yandex.ru>
2025-06-26 00:39:44 +0700

    fix: деплой

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from 'react-router-dom'; // обязательно react-router-dom
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // обязательно react-router-dom
 import Layout from './components/Layout';
 
-import { HabitsListPage } from './pages/HabitsListPage/HabitsListPage.tsx';
+import { HabitsListPage } from './pages/HabitsListPage/HabitsListPage';
 import { HabitDetailPage } from './pages/HabitDetailPage/HabitDetailPage';
 
 const App: React.FC = () => {
     return (
-        <Layout>
-            <Routes>
-                <Route path="/" element={<HabitsListPage />} />
-                <Route path="/habits/:id" element={<HabitDetailPage />} />
-            </Routes>
-        </Layout>
+        <BrowserRouter basename="/Todo">
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<HabitsListPage />} />
+                    <Route path="/habits/:id" element={<HabitDetailPage />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
     );
 };
 
