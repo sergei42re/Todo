9791fd80827c18e0c1f4174b820f5c4abff2875b
sergei <sergei42ru.alekseenko@yandex.ru>
2025-06-26 00:23:24 +0700

    fix: деплой

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Todo/', // 👈 обязательно с косыми чертами
   plugins: [react()],
-})
+});
